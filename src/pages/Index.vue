e906191b1416f10740fe378ac6ab29e2b2bda1da
jonathan.rosado <jonathan.rosado@eurecia.com>
2021-04-08 16:07:27 +0200

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn @click="callHaptics()">
+      haptic test
+    </q-btn>
+  </q-page>
+</template>
+
+<script>
+import { Haptics, ImpactStyle, NotificationType } from '@capacitor/haptics';
+
+export default {
+  name: 'PageIndex',
+
+  methods:  {
+    callHaptics() { 
+      Haptics.impact({ style: NotificationType.SUCCESS });
+    }
+  }
+}
+</script>
